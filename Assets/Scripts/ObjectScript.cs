ef3bb4678b0b3227a5beb00bc0fa616ec97c4f68
123gleb123 <gchechotkin@gmail.com>
2025-05-26 02:31:00 +0300

    Pievienoju jaunas mašinas ObjectScriptā un PlaceSkriptā

--- Assets/Scripts/ObjectScript.cs
+++ Assets/Scripts/ObjectScript.cs
@@ -7,25 +7,47 @@ public class ObjectScript : MonoBehaviour
     public GameObject garbageTruck;
     public GameObject schoolBuss;
     public GameObject medic;
+    public GameObject ekskavator;
+    public GameObject e46;
+    public GameObject b2;
+    public GameObject e61;
+    public GameObject police;
+    public GameObject traktors1;
+    public GameObject traktos5;
+    public GameObject cementaMasina;
+    public GameObject ugunsdzeSeji;
 
-    [HideInInspector]
-    public Vector2 gTruckPos;
-    [HideInInspector]
-    public Vector2 sBussPos;
-    [HideInInspector]
-    public Vector2 medicPos;
+    [HideInInspector] public Vector2 gTruckPos;
+    [HideInInspector] public Vector2 sBussPos;
+    [HideInInspector] public Vector2 medicPos;
+    [HideInInspector] public Vector2 ekskavatorPos;
+    [HideInInspector] public Vector2 e46Pos;
+    [HideInInspector] public Vector2 b2Pos;
+    [HideInInspector] public Vector2 e61Pos;
+    [HideInInspector] public Vector2 policePos;
+    [HideInInspector] public Vector2 traktors1Pos;
+    [HideInInspector] public Vector2 traktos5Pos;
+    [HideInInspector] public Vector2 cementaMasinaPos;
+    [HideInInspector] public Vector2 ugunsdzeSejiPos;
 
     public AudioSource audioSource;
     public AudioClip[] audioClips;
-    [HideInInspector]
-    public bool rightPlace = false;
+    [HideInInspector] public bool rightPlace = false;
     public GameObject lastDragged = null;
-
 
     void Start()
     {
         gTruckPos = garbageTruck.GetComponent<RectTransform>().localPosition;
         sBussPos = schoolBuss.GetComponent<RectTransform>().localPosition;
         medicPos = medic.GetComponent<RectTransform>().localPosition;
+        ekskavatorPos = ekskavator.GetComponent<RectTransform>().localPosition;
+        e46Pos = e46.GetComponent<RectTransform>().localPosition;
+        b2Pos = b2.GetComponent<RectTransform>().localPosition;
+        e61Pos = e61.GetComponent<RectTransform>().localPosition;
+        policePos = police.GetComponent<RectTransform>().localPosition;
+        traktors1Pos = traktors1.GetComponent<RectTransform>().localPosition;
+        traktos5Pos = traktos5.GetComponent<RectTransform>().localPosition;
+        cementaMasinaPos = cementaMasina.GetComponent<RectTransform>().localPosition;
+        ugunsdzeSejiPos = ugunsdzeSeji.GetComponent<RectTransform>().localPosition;
     }
 }
--- Assets/Scripts/PlaceScript.cs
+++ Assets/Scripts/PlaceScript.cs
@@ -15,88 +15,92 @@ public class PlaceScript : MonoBehaviour, IDropHandler
         if ((eventData.pointerDrag != null) && Input.GetMouseButtonUp(0)
             && Input.GetMouseButton(2) == false)
         {
-
             if (eventData.pointerDrag.tag.Equals(tag))
             {
-                placeZRotation =
-                eventData.pointerDrag.GetComponent<RectTransform>().transform.eulerAngles.z;
-
+                // Salīdzina rotāciju starp objektu un vietu
+                placeZRotation = eventData.pointerDrag.GetComponent<RectTransform>().transform.eulerAngles.z;
                 carZRotation = GetComponent<RectTransform>().transform.eulerAngles.z;
-
                 difZRotation = Mathf.Abs(placeZRotation - carZRotation);
                 Debug.Log("Dif Z Rotation: " + difZRotation);
 
+                // Salīdzina izmēru starp objektu un vietu
                 placeSize = eventData.pointerDrag.GetComponent<RectTransform>().localScale;
                 carSize = GetComponent<RectTransform>().localScale;
                 xSizeDif = Mathf.Abs(placeSize.x - carSize.x);
                 ySizeDif = Mathf.Abs(placeSize.y - carSize.y);
                 Debug.Log("Dif X Size: " + xSizeDif + " Dif Y Size: " + ySizeDif);
 
-                if ((difZRotation <=10 || (difZRotation >=350 && difZRotation <=360)) && 
-                    (xSizeDif <= 0.3 && ySizeDif <= 0.3)){
+                if ((difZRotation <= 10 || (difZRotation >= 350 && difZRotation <= 360)) &&
+                    (xSizeDif <= 0.3 && ySizeDif <= 0.3))
+                {
                     Debug.Log("Right Place");
                     objectScript.rightPlace = true;
 
-                    // Iecentrē pozīciju
+                    // Objektu novieto precīzā vietā
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition =
                         GetComponent<RectTransform>().anchoredPosition;
 
-                    // Pielāgo rotāciju
-                    eventData.pointerDrag.GetComponent<RectTransform>().localRotation = 
+                    // Uzliek rotāciju pēc vietas
+                    eventData.pointerDrag.GetComponent<RectTransform>().localRotation =
                         GetComponent<RectTransform>().localRotation;
 
-                    // Pielāgo izmēru
-                    eventData.pointerDrag.GetComponent<RectTransform>().localScale = 
+                    // Uzliek izmēru pēc vietas
+                    eventData.pointerDrag.GetComponent<RectTransform>().localScale =
                         GetComponent<RectTransform>().localScale;
 
-                    switch(eventData.pointerDrag.tag)
-                    {
-                        case "Garbage":
-                            objectScript.audioSource.PlayOneShot(objectScript.audioClips[2]);
-                            break;
-
-                        case "Medic":
-                            objectScript.audioSource.PlayOneShot(objectScript.audioClips[4]);
-                            break;
-
-                        case "School":
-                            objectScript.audioSource.PlayOneShot(objectScript.audioClips[3]);
-                            break;
-
-                        default:
-                            Debug.LogError("Unknown tag!");
-                            break;
-                    }
+                    // Atskaņo veiksmīgas novietošanas skaņu
+                    objectScript.audioSource.PlayOneShot(objectScript.audioClips[0]);
                 }
-
             }
             else
             {
+                // Ja nepareizā vietā — atgriež atpakaļ un atskaņo kļūdas skaņu
                 objectScript.rightPlace = false;
                 objectScript.audioSource.PlayOneShot(objectScript.audioClips[1]);
 
+                // Atgriež objektu sākotnējā pozīcijā pēc tag
                 switch (eventData.pointerDrag.tag)
                 {
                     case "Garbage":
-                        objectScript.garbageTruck.GetComponent<RectTransform>().localPosition 
-                            = objectScript.gTruckPos;
+                        objectScript.garbageTruck.GetComponent<RectTransform>().localPosition = objectScript.gTruckPos;
                         break;
-
                     case "Medic":
-                        objectScript.medic.GetComponent<RectTransform>().localPosition
-                            = objectScript.medicPos;
+                        objectScript.medic.GetComponent<RectTransform>().localPosition = objectScript.medicPos;
                         break;
-
                     case "School":
-                        objectScript.schoolBuss.GetComponent<RectTransform>().localPosition
-                             = objectScript.sBussPos;
+                        objectScript.schoolBuss.GetComponent<RectTransform>().localPosition = objectScript.sBussPos;
                         break;
-
+                    case "Ekskavator":
+                        objectScript.ekskavator.GetComponent<RectTransform>().localPosition = objectScript.ekskavatorPos;
+                        break;
+                    case "e46":
+                        objectScript.e46.GetComponent<RectTransform>().localPosition = objectScript.e46Pos;
+                        break;
+                    case "b2":
+                        objectScript.b2.GetComponent<RectTransform>().localPosition = objectScript.b2Pos;
+                        break;
+                    case "e61":
+                        objectScript.e61.GetComponent<RectTransform>().localPosition = objectScript.e61Pos;
+                        break;
+                    case "Police":
+                        objectScript.police.GetComponent<RectTransform>().localPosition = objectScript.policePos;
+                        break;
+                    case "Traktors1":
+                        objectScript.traktors1.GetComponent<RectTransform>().localPosition = objectScript.traktors1Pos;
+                        break;
+                    case "Traktos5":
+                        objectScript.traktos5.GetComponent<RectTransform>().localPosition = objectScript.traktos5Pos;
+                        break;
+                    case "CementaMasina":
+                        objectScript.cementaMasina.GetComponent<RectTransform>().localPosition = objectScript.cementaMasinaPos;
+                        break;
+                    case "UgunsdzeSeji":
+                        objectScript.ugunsdzeSeji.GetComponent<RectTransform>().localPosition = objectScript.ugunsdzeSejiPos;
+                        break;
                     default:
                         Debug.LogError("Unknown tag!");
                         break;
                 }
-
             }
         }
     }
