ff2240b0ca0c471d5e8d43d0b30df9bba1f8e3a3
123gleb123 <gchechotkin@gmail.com>
2025-05-26 00:17:41 +0300

    Pabeidzu taisit Menue

---
+++ Assets/Scripts/SceneChangeScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeScript : MonoBehaviour
+{
+    public void ToUIScene()
+    {
+        SceneManager.LoadScene("CityScene", LoadSceneMode.Single);
+    }
+
+    public void ToSecondUIScene()
+    {
+        SceneManager.LoadScene("CityScene2", LoadSceneMode.Single);
+    }
+
+    public void ToMainMenue()
+    {
+        SceneManager.LoadScene("Menue", LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
+
